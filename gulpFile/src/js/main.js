81594c864d6e5d4460fc5aa779f7069f491c0573
kei.igarashi <>
2021-08-05 15:09:02 +0900

    一旦clipboradが動いたところまで

--- gulpFile/src/js/main.js
+++ gulpFile/src/js/main.js
@@ -1,5 +1,9 @@
 // import 文を使って sub.js ファイルを読み込む。
 import { hello } from "./sub";
 
+/** Import Page Module */ // パスの昇順で記載する
+import {copyUrlAction} from './module/copy_url';
+
 // sub.jsに定義されたJavaScriptを実行する。
-hello();
+// hello();
+copyUrlAction('.btn');
---
+++ gulpFile/src/js/module/copy_url.js
@@ -0,0 +1,16 @@
+import ClipboardJS from '../plugin/clipboard.min';
+
+/**
+ * ターゲット要素押下でdata属性に指定された文言をクリップボードに保存する
+ * @param {String} target
+ * @constructor
+ */
+
+export function copyUrlAction(target) {
+    let clipboard = new ClipboardJS(target);
+    let alertComment = 'URLをコピーしました';
+
+    clipboard.on('success', function() {
+        alert(alertComment);
+    });
+};
